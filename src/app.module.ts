2ee588d6d4477b8891971b3ae097694d6aa0220e
fjbalsamo <fjbalsamo@gmail.com>
2021-03-16 14:46:21 -0300

    docker-compose created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,37 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ProvidersModule } from './providers/providers.module';
 
+import { CategoryEntity } from './categories/entity';
+import { ProductEntity } from './products/entity';
+import { ProviderEntity } from './providers/entity'
+
+const OrmModule = TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'db_serempre',
+  synchronize: true,
+  entities: [
+    CategoryEntity,
+    ProviderEntity,
+    ProductEntity
+  ]
+})
+
 @Module({
-  imports: [ProductsModule, CategoriesModule, ProvidersModule],
+  imports: [
+    OrmModule,
+    ProductsModule, 
+    CategoriesModule, 
+    ProvidersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
